697b42e8245ab568d3e8a9353dc0ba0fc7e782d1
kindavishal <vishaldas.95@gmail.com>
2019-07-02 23:36:48 +0530

    Presentational and Container Components

--- src/components/dishDetailsComponent.js
+++ src/components/dishDetailsComponent.js
@@ -8,8 +8,8 @@ class DishDetail extends Component{
             const dishComments = dish.comments.map((comment) => {    
                 return (
                     <div>
-                        <li>{comment.comment}</li>
-                        <li>-- {comment.author}, {comment.date}</li>
+                        <p>{comment.comment}</p>
+                        <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                     </div>
                 );
               });              
@@ -34,22 +34,24 @@ class DishDetail extends Component{
 }
 
     render() {
-        const dish = this.props.selectedDish;
+        const dish = this.props.dish;
 
         if (dish != null)
             return(
-                <div className = 'row'>
-                    <div className = 'col-12 col-md-5 m-1'>
-                        <Card>
-                            <CardImg top src={dish.image} alt={dish.name} />
-                            <CardBody>
-                                <CardTitle>{dish.name}</CardTitle>
-                                <CardText>{dish.description}</CardText>
-                            </CardBody>
-                        </Card>
-                    </div>
-                    <div className = 'col-12 col-md-5 m-1'>
-                        {this.renderComments(this.props.selectedDish)}
+                <div className = 'container'>
+                    <div className = 'row'>
+                        <div className = 'col-12 col-md-5 m-1'>
+                            <Card>
+                                <CardImg top src={dish.image} alt={dish.name} />
+                                <CardBody>
+                                    <CardTitle>{dish.name}</CardTitle>
+                                    <CardText>{dish.description}</CardText>
+                                </CardBody>
+                            </Card>
+                        </div>
+                        <div className = 'col-12 col-md-5 m-1'>
+                            {this.renderComments(this.props.dish)}
+                        </div>
                     </div>
                 </div>
             );
